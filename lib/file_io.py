fe67873cb0109be4c22ec0bd8b4a4f0e810080d3
Morinke01 <ejulius250@gmail.com>
2023-12-07 15:43:07 +0300

    Assignment completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
+    print("File written successfully!")
 
+write_file(file_name="file", file_content="This is a test content.")
+   
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", 'a') as file:
+        file.write(append_content)
+    print("Content appended successfully!")
+
+append_file(file_name="file", append_content="This is a test content.")
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", 'r') as file:
+            content = file.read()
+    return content
+    
